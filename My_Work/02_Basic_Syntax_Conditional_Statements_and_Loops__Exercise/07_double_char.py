3f31e6f2fd7c0811bc0310478d1984e8af9e0956
Anton Petrov <a.petrov.83@gmail.com>
2022-08-28 11:26:46 +0300

    Added exam "07. Double Char" from "Basic Syntax, Conditional Statements and Loops - Exercise"

---
+++ My_Work/02_Basic_Syntax_Conditional_Statements_and_Loops__Exercise/07_double_char.py
@@ -0,0 +1,14 @@
+string = input()
+
+while string != 'End':
+
+    if string == 'SoftUni':
+        string = input()
+        continue
+
+    for letter in string:
+        print(letter * 2, end='')
+
+    print()
+
+    string = input()
