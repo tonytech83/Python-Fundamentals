34a17fa4d23c759b7fd7d42a4001cb58ea59d072
Anton Petrov <a.petrov.83@gmail.com>
2022-09-22 09:58:43 +0300

    Added a solution that does not depend on the length of the variable number

--- 04_Data_Types_and_Variables_Lab/03_special_numbers.py
+++ 04_Data_Types_and_Variables_Lab/03_special_numbers.py
@@ -1,13 +1,18 @@
 number = int(input())
+len_of_number = len(str(number))
 
 for num in range(1, number + 1):
 
+    sum_of_digits = 0
     flag = False
 
-    num = str(num).zfill(6)
-    sum_of_digits = int(num[0]) + int(num[1]) + int(num[2]) + int(num[3]) + int(num[4]) + int(num[5])
+    num = str(num)
+
+    for digit in num:
+        sum_of_digits += int(digit)
+
     if sum_of_digits == 5 or sum_of_digits == 7 or sum_of_digits == 11:
-        flag = 'True'
+        flag = True
         sum_of_digits = 0
 
     print(f'{int(num)} -> {flag}')
