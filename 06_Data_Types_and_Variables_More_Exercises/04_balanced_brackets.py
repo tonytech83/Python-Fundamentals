b33282f74d288adbc25828fe68f0b9f1baff56fc
Anton Petrov <a.petrov.83@gmail.com>
2022-09-24 11:16:55 +0300

    Added exam "04. Balanced Brackets" from "Data Types and Variables - More Exercises"

---
+++ 06_Data_Types_and_Variables_More_Exercises/04_balanced_brackets.py
@@ -0,0 +1,24 @@
+number_of_lines = int(input())
+
+bracket_array = [None]
+
+is_balanced = True
+
+for line in range(number_of_lines):
+
+    char = input()
+
+    if char == "(" or char == ")":
+
+        if char == bracket_array[-1]:
+            is_balanced = False
+            break
+
+        bracket_array.append(char)
+    else:
+        continue
+
+if is_balanced and bracket_array[1] == "(":
+    print(f'BALANCED')
+else:
+    print('UNBALANCED')
