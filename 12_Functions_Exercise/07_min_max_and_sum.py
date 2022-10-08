6324a57310259378112581f1f18e66ab765517ee
Anton Petrov <a.petrov.83@gmail.com>
2022-10-08 10:02:47 +0300

    Added exam "07. Min Max and Sum" from "Functions - Exercise"

---
+++ 12_Functions_Exercise/07_min_max_and_sum.py
@@ -0,0 +1,9 @@
+sequence_of_numbers = [int(x) for x in input().split()]
+
+minimum_number = min(sequence_of_numbers)
+maximum_number = max(sequence_of_numbers)
+sum_of_all_numbers = sum(sequence_of_numbers)
+
+print(f'The minimum number is {minimum_number}')
+print(f'The maximum number is {maximum_number}')
+print(f'The sum number is: {sum_of_all_numbers}')
