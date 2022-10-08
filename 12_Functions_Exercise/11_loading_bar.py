0f0b22838c4b34f341904e90616619a6229677e3
Anton Petrov <a.petrov.83@gmail.com>
2022-10-08 17:53:32 +0300

    Added exam "11. Loading Bar" from "Functions - Exercise" - made changes on func and print

--- 12_Functions_Exercise/11_loading_bar.py
+++ 12_Functions_Exercise/11_loading_bar.py
@@ -1,18 +1,15 @@
 def loading_bar(number, bar):
-    if number == 100:
-        return bar
-    else:
-        for idx in range(0, 100 - number, 10):
-            bar.remove("%")
-            bar.append('.')
-        return bar
+    for _ in range(0, 100 - number, 10):
+        bar.remove("%")
+        bar.append('.')
+    return bar
 
 
-bar = ['%'] * 10
 percent_number = int(input())
-loading_bar(percent_number, bar)
 
 if percent_number == 100:
     print(f'{percent_number}% Complete!\n[%%%%%%%%%%]')
 else:
+    bar = ['%'] * 10
+    loading_bar(percent_number, bar)
     print(f"{percent_number}% [{''.join(bar)}]\nStill loading...")
