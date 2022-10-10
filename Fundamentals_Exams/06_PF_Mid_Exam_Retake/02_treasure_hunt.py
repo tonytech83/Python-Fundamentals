10e31f15254e044c79dbf3c8b818d0211dabce34
Anton Petrov <a.petrov.83@gmail.com>
2022-10-10 16:21:11 +0300

    Added exam "02. Treasure Hunt" from "06. Programming Fundamentals Mid Exam Retake" - added comments and docstring in all functions

--- Fundamentals_Exams/06_PF_Mid_Exam_Retake/02_treasure_hunt.py
+++ Fundamentals_Exams/06_PF_Mid_Exam_Retake/02_treasure_hunt.py
@@ -1,15 +1,33 @@
 def loot(command, initial_loot):
+    """
+    :param command: receive input from customer
+    :param initial_loot: list of items received by customer
+    :list current_lot: split the variable to list of items
+    :list new_lot: exclude event (the first element) and add only the new items in new_lot.
+    :return: initial_list after adding only non-contained items
+    """
     current_lot = command.split()
     new_lot = current_lot[1:]
-    for item in new_lot:
+    # take all items one by one from new_lot list
+    for new_item in new_lot:
+        # if item not in initial_loot list insert the item at the beginning
         if item not in initial_loot:
-            initial_loot.insert(0, item)
+            initial_loot.insert(0, new_item)
     return initial_loot
 
 
 def drop(command, initial_loot):
+    """
+    :param command: receive input from customer
+    :param initial_loot: list of items received by customer
+    :list drop_event: split the variable to list of items
+    :variable idx - the index of item which should be removed
+    :return: initial_list after remove the item by index and add it at the end of the list
+    """
     drop_event = command.split()
     idx = int(drop_event[1])
+    # check if index is inside of initial_loot list.
+    # if yes remove the item and add it at the end of the list
     if 0 <= idx <= len(initial_loot):
         removed_item = initial_loot.pop(idx)
         initial_loot.append(removed_item)
@@ -17,12 +35,22 @@ def drop(command, initial_loot):
 
 
 def steal(command, initial_loot):
+    """
+    :param command: receive input from customer
+    :param initial_loot: list of items received by customer
+    :list steal_event: split the variable to list of items
+    :variable count: the count of items which should be removed
+    :list stolen_items: contained all items removed from end of initial_loot list
+    :return: print stolen_items list and return initial_loot
+    """
     steal_event = command.split()
     count = int(steal_event[1])
     stolen_items = []
+    # remove item per iteration in range of count and add it to stolen_items list
     for item in range(count):
         stolen_item = initial_loot.pop()
         stolen_items.insert(0, stolen_item)
+        # check if initial_loot is empty
         if not initial_loot:
             break
     return print(*stolen_items, sep=', '), initial_loot
@@ -47,13 +75,15 @@ while command != 'Yohoho!':
 
     command = input()
 
+# check initial_loot list is empty
 if not initial_loot:
     print('Failed treasure hunt.')
+# if the initial_loot is not empty, take the length items one by one and add it to total_gain variable
 else:
     total_gain = 0
     for item in initial_loot:
         total_gain += len(item)
-
+    # delete the total length of elements in initial_loot list and divide it to length of initial_loot list
+    # result is average gain
     average_gain = total_gain / len(initial_loot)
-
     print(f'Average treasure gain: {average_gain:.2f} pirate credits.')
