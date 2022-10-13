5be5b5ccb68e4594695e7fc1399a7a7b5196b452
Anton Petrov <a.petrov.83@gmail.com>
2022-10-13 20:00:13 +0300

    Added exam "02. Trains" from "Lists Advanced - Lab"

---
+++ 14_Lists_Advanced_Lab/02_trains.py
@@ -0,0 +1,39 @@
+def add_people(current_command):
+    global train
+    people = int(current_command[1])
+    train[-1] += people
+
+
+def insert_people(current_command):
+    global train
+    wagon = int(current_command[1])
+    people = int(current_command[2])
+    train[wagon] += people
+
+
+def remove_people(current_command):
+    global train
+    wagon = int(current_command[1])
+    people = int(current_command[2])
+    train[wagon] -= people
+
+
+number_wagons = int(input())
+
+train = [0] * number_wagons
+
+command = input()
+
+while command != 'End':
+    current_command = command.split(' ')
+    event = current_command[0]
+
+    if event == 'add':
+        add_people(current_command)
+    elif event == 'insert':
+        insert_people(current_command)
+    elif event == 'leave':
+        remove_people(current_command)
+
+    command = input()
+print(train)
