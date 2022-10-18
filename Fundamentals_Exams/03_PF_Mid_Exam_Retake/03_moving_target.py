3db5bcbf1af4c6f1ec57e069b657b9522ee85406
Anton Petrov <a.petrov.83@gmail.com>
2022-10-18 21:24:40 +0300

    made change on input and parameters in if-elif construction

--- Fundamentals_Exams/03_PF_Mid_Exam_Retake/03_moving_target.py
+++ Fundamentals_Exams/03_PF_Mid_Exam_Retake/03_moving_target.py
@@ -5,37 +5,35 @@ for value in input().split(' '):
 
 while True:
 
-    command = input().split(' ')
+    command = input()
+
+    if command == 'End':
+        print(*targets, sep='|')
+        break
+
+    event, index, value = command.split()
+    index = int(index)
+    value = int(value)
 
     # check if command is Shoot
-    if command[0] == 'Shoot':
-        index = int(command[1])
-        value = int(command[2])
+    if event == 'Shoot':
         if 0 <= index < len(targets):
             targets[index] -= value
             if targets[index] <= 0:
                 targets.pop(index)
 
     # check if command is Add
-    elif command[0] == 'Add':
-        index = int(command[1])
-        value = int(command[2])
+    elif event == 'Add':
         if 0 <= index < len(targets):
             targets.insert(index, value)
         else:
             print('Invalid placement!')
 
     # check if command is Strike
-    elif command[0] == 'Strike':
-        index = int(command[1])
-        radius = int(command[2])
-        left_radius = index - radius
-        right_radius = index + radius
+    elif event == 'Strike':
+        left_radius = index - value
+        right_radius = index + value
         if left_radius >= 0 and right_radius < len(targets):
             del targets[left_radius: right_radius + 1]
         else:
             print('Strike missed!')
-
-    elif command[0] == 'End':
-        print(*targets, sep='|')
-        break
