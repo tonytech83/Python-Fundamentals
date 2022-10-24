6dacc19f9ebf2cc9f46a00cb34d06db7b91e53b6
Anton Petrov <a.petrov.83@gmail.com>
2022-10-24 21:26:01 +0300

    Added exam "05. Circle" from "Objects and Classes - Lab"

---
+++ 18_Objects_and_Classes_Lab/05_circle.py
@@ -0,0 +1,23 @@
+class Circle:
+    __pi = 3.14
+
+    def __init__(self, diameter):
+        self.diameter = diameter
+
+    def calculate_circumference(self):
+        return self.__pi * self.diameter
+
+    def calculate_area(self):
+        return self.__pi * self.diameter ** 2 / 4
+
+    def calculate_area_of_sector(self, angle):
+        return angle / 360 * self.__pi * (self.diameter / 2) ** 2
+
+
+############# Test code ###########
+circle = Circle(10)
+angle = 5
+
+print(f"{circle.calculate_circumference():.2f}")
+print(f"{circle.calculate_area():.2f}")
+print(f"{circle.calculate_area_of_sector(angle):.2f}")
