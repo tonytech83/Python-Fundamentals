acaaed54c9e0499fe2b758e29aeb5f54214edcd1
Anton Petrov <a.petrov.83@gmail.com>
2022-10-25 21:24:17 +0300

    Added exam "06. Inventory" from "Objects and Classes - Exercise"

---
+++ 19_Objects_and_Classes_Exercise/06_inventory.py
@@ -0,0 +1,26 @@
+class Inventory:
+
+    def __init__(self, capacity: int):
+        self.__capacity = capacity
+        self.items = []
+
+    def add_item(self, item: str):
+        if len(self.items) >= self.__capacity:
+            return 'not enough room in the inventory'
+        else:
+            self.items.append(item)
+
+    def get_capacity(self):
+        return self.__capacity
+
+    def __repr__(self):
+        return f'Items: {", ".join(self.items)}.\nCapacity left: {self.get_capacity() - len(self.items)}'
+
+
+# test code
+inventory = Inventory(2)
+inventory.add_item("potion")
+inventory.add_item("sword")
+print(inventory.add_item("bottle"))
+print(inventory.get_capacity())
+print(inventory)
