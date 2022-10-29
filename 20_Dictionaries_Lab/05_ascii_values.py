f487eec9b30c6fca58916a7a0bf67a9ac2055e2a
Anton Petrov <a.petrov.83@gmail.com>
2022-10-29 21:42:47 +0300

    Added second solution with one comprehension

--- 20_Dictionaries_Lab/05_ascii_values.py
+++ 20_Dictionaries_Lab/05_ascii_values.py
@@ -2,3 +2,6 @@ lst_of_chr = [x for x in input().split(', ')]
 lst_of_ascii = [ord(x) for x in lst_of_chr]
 
 print(dict(zip(lst_of_chr, lst_of_ascii)))
+
+# lst_of_chr = input().split(', ')
+# print({char: ord(char) for char in lst_of_chr})
