23e20d818880654a3ee81ae7637b681bb2b6b259
Anton Petrov <a.petrov.83@gmail.com>
2022-10-30 18:03:18 +0200

    Added solution for exam "10. Company Users" from "Dictionaries - Exercise"

---
+++ 21_Distionaries_Exercise/10_company_users.py
@@ -0,0 +1,19 @@
+company_users = {}
+
+while True:
+    line = input()
+    if line == 'End':
+        break
+
+    company_name, employee_id = line.split(' -> ')
+    if company_name not in company_users:
+        company_users[company_name] = []
+        company_users[company_name].append(employee_id)
+    else:
+        if employee_id not in company_users[company_name]:
+            company_users[company_name].append(employee_id)
+
+for name, employees in company_users.items():
+    print(name)
+    for user in employees:
+        print(f'-- {user}')
