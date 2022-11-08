fac92cc9dfe6071c5ad2e8e36c778e9a895b7b06
Anton Petrov <a.petrov.83@gmail.com>
2022-11-08 16:05:49 +0200

    Added 2 more solutions for exam "04. Caesar Cipher" from "Text Processing - Exercise" with comprehension

--- 25_Text_Processing_Exercise/04_caesar_cipher.py
+++ 25_Text_Processing_Exercise/04_caesar_cipher.py
@@ -6,3 +6,8 @@ for char in text:
     encrypted_text += chr(ord(char) + 3)
 
 print(encrypted_text)
+
+# solution 1 with comprehension
+# print(*[chr(ord(char) + 3) for char in text], sep='')
+# solution 2 with comprehension
+# print(''.join([chr(ord(char) + 3) for char in text]))
