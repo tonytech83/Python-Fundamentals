87d12d35bd16dde7cc2f9bd41a694fc5c9bddf4f
Anton Petrov <a.petrov.83@gmail.com>
2022-11-08 18:24:40 +0200

    Added solution for exam "07. String Explosion" from "Text Processing - Exercise"

---
+++ 25_Text_Processing_Exercise/07_string_explosion.py
@@ -0,0 +1,30 @@
+sequence = input()
+
+new_sequence = ''
+
+strength = 0
+
+while '>' in sequence:
+    if not sequence:
+        break
+
+    if sequence[0] == '>':
+        new_sequence += '>'
+        sequence = sequence[1:]
+        if not sequence[0].isdigit():
+            break
+        strength += int(sequence[0])
+        for remove in range(1, strength + 1):
+            sequence = sequence[1:]
+            strength -= 1
+            if not sequence:
+                break
+            if sequence[0] == '>':
+                break
+
+    else:
+        char = sequence[:1]
+        new_sequence += char
+        sequence = sequence[1:]
+
+print(new_sequence + sequence)
