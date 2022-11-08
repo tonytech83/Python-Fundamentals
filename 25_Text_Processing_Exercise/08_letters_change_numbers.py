14c95f46d18b49aedd1737b1d45732824b00a334
Anton Petrov <a.petrov.83@gmail.com>
2022-11-08 20:17:29 +0200

    Added solution for exam "08. Letters Change Numbers" from "Text Processing - Exercise"

--- 25_Text_Processing_Exercise/08_letters_change_numbers.py
+++ 25_Text_Processing_Exercise/08_letters_change_numbers.py
@@ -1,3 +1,23 @@
+def first_letter_check(first_letter, current_result):
+    if first_letter.isupper():
+        first_letter = ord(string[0]) - 64
+        current_result += number / first_letter
+    else:
+        first_letter = ord(string[0]) - 96
+        current_result += number * first_letter
+    return current_result
+
+
+def last_letter_check(last_letter, current_result):
+    if last_letter.isupper():
+        last_letter = ord(string[-1]) - 64
+        current_result -= last_letter
+    else:
+        last_letter = ord(string[-1]) - 96
+        current_result += last_letter
+    return current_result
+
+
 strings = input().split()
 
 result = 0
@@ -6,20 +26,8 @@ for string in strings:
     number = int(''.join([x for x in string if x.isdigit()]))
     string_result = 0
 
-    if string[0].isupper():
-        first_letter = ord(string[0]) - 64
-        string_result += number / first_letter
-    else:
-        first_letter = ord(string[0]) - 96
-        string_result += number * first_letter
-
-    if string[-1].isupper():
-        last_letter = ord(string[-1]) - 64
-        string_result = string_result - last_letter
-    else:
-        last_letter = ord(string[-1]) - 96
-        string_result = string_result + last_letter
-
+    intermediate_result = first_letter_check(string[0], string_result)
+    string_result = last_letter_check(string[-1], intermediate_result)
     result += string_result
 
 print(f'{result:.2f}')
