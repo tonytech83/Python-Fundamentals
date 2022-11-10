4958d0f7ffaa24d7080f651f9ad2c49ea09bd48d
Anton Petrov <a.petrov.83@gmail.com>
2022-11-10 20:19:28 +0200

    Added solution for exam "2.	Bit at Position 1" from "Bitwise Operations - Lab"

--- 24_Bitwise_Operations/01_binary_digits_count.py
+++ 24_Bitwise_Operations/01_binary_digits_count.py
@@ -1,12 +1,12 @@
 number = int(input())
-binary_digit = input()
+zero_or_one = input()
 
 binary_text = 'ones'
-if binary_digit == 0:
+if zero_or_one == 0:
     binary_text = 'zeroes'
 
 binary_number = bin(number)[2:]
-binary_digit_count = binary_number.count(binary_digit)
+binary_digit_count = binary_number.count(zero_or_one)
 
 print(f'{number} - > {binary_number}')
 print(f'We have {binary_digit_count} {binary_text}')
---
+++ 24_Bitwise_Operations/02_bit_at_position_1.py
@@ -0,0 +1,4 @@
+number = int(input())
+number_to_binary = bin(number)[2:]
+
+print(number_to_binary[-2])
