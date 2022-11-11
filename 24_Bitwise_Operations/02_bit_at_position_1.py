bd175482e8bcedef98a8df7ce502462f015f59af
Anton Petrov <a.petrov.83@gmail.com>
2022-11-11 09:57:35 +0200

    Added solution for exam "2.	Bit at Position 1" from "Bitwise Operations - Lab" with bitwise operation

--- 24_Bitwise_Operations/02_bit_at_position_1.py
+++ 24_Bitwise_Operations/02_bit_at_position_1.py
@@ -1,4 +1,8 @@
 number = int(input())
-number_to_binary = bin(number)[2:]
 
-print(number_to_binary[-2])
+# Shift the number n times to the right (where n is the position, in this case, it is 1) by using the >> operator.
+# In that way, the bit we want to check will be at position 0;
+number_to_binary = bin(number >> 1)
+
+# Find and print the bit at position 0
+print(number_to_binary[-1])
