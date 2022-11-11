1b4f392f9d6f22c3c99e9e7cfca7c33e6607d643
Anton Petrov <a.petrov.83@gmail.com>
2022-11-11 20:22:26 +0200

    Changed solution for exam "03. Extract File" from "Text Processing - Exercise" - take two variables after initial split of input

--- 25_Text_Processing_Exercise/03_extract_file.py
+++ 25_Text_Processing_Exercise/03_extract_file.py
@@ -1,5 +1,4 @@
 path = input().split('\\')
-file = path[-1].split('.')
+filename, extension = path[-1].split('.')
 
-print(f'File name: {file[0]}\nFile extension: {file[1]}')
-
+print(f'File name: {filename}\nFile extension: {extension}')
