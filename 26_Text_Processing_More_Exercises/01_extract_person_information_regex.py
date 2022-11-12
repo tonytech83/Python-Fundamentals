cc61ec560be6b65818416c76d0d9ab95324609fe
Anton Petrov <a.petrov.83@gmail.com>
2022-11-12 17:55:24 +0200

    Added solution with regex for exam "01. Extract Person Information" from "Text Processing - More Exercises"

---
+++ 26_Text_Processing_More_Exercises/01_extract_person_information_regex.py
@@ -0,0 +1,10 @@
+import re
+
+number_of_lines = int(input())
+
+for line in range(number_of_lines):
+    data = input()
+    name = re.findall(r'\@([A-Za-z]\w*)\b', data)
+    age = re.findall(r'\#(\d*\b)', data)
+    if name and age:
+        print(f'{name[0]} is {age[0]} years old.')
