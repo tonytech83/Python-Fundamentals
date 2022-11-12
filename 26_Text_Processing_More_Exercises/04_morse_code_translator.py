0a28c2f4187967246bc216ac439f66636edc43ad
Anton Petrov <a.petrov.83@gmail.com>
2022-11-12 16:08:01 +0200

    Added solution for exam "04. Morse Code Translator" from "Text Processing - More Exercises"

---
+++ 26_Text_Processing_More_Exercises/04_morse_code_translator.py
@@ -0,0 +1,27 @@
+morse_code_dict = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+
+message = input().split(' | ')
+translated_message = []
+for word in message:
+    characters = word.split()
+    for char in characters:
+        for key, value in morse_code_dict.items():
+            if char == value:
+                translated_message.append(key)
+                break
+    translated_message.append(' ')
+print(*translated_message, sep='')
