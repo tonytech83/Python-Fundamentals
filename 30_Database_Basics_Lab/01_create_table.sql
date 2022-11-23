566de11ed93565da90cf43433e02d58c32938b1b
Anton Petrov <a.petrov.83@gmail.com>
2022-11-23 16:47:14 +0200

    Added all solutions from "Database Basics - Lab"

---
+++ 30_Database_Basics_Lab/01_create_table.sql
@@ -0,0 +1,9 @@
+USE `softuni`;
+
+CREATE TABLE `students` (
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+    `first_name` VARCHAR(50),
+    `last_name` VARCHAR(50),
+    `age` INT,
+    `grade` DOUBLE
+);
